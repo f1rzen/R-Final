b48eb1107098e618e7af096da0af57bf54c52216
Ash <92748453+AshNumpy@users.noreply.github.com>
2021-12-28 16:27:12 +0300

    Project

--- Project/script.R
+++ Project/script.R
@@ -7,7 +7,7 @@ library(tidyr)
 a <-corona %>%
   filter(continent %in% c("Africa","Asia","Europe","North America","South America")) %>%
   summarize(date=date,continent=continent, location=location, new_cases=new_cases, total_cases=total_cases, new_cases_per_million=new_cases_per_million,
-            new_deaths=new_deaths, total_deaths=total_deaths, new_deaths_per_million=new_deaths_per_million, population=population,cases_density_population=total_cases/population)
+            new_deaths=new_deaths,date=date, total_deaths=total_deaths, new_deaths_per_million=new_deaths_per_million, population=population,cases_density_population=total_cases/population)
 
 a <- a%>%
   filter(date > "2020-06-01" & date <"2020-06-30") %>%
@@ -31,11 +31,12 @@ x_sth_america <- South_America[sample(nrow(South_America),20),]
 x_nrth_america <- North_America[sample(nrow(North_America),20),]
 
 x_corona <- rbind(x_asia,x_africa,x_europe,x_sth_america,x_nrth_america)
-x_corona <- x_corona[,-1]
 
 library(writexl)
-write.csv(x_corona, "C:\\Users\\lenovo\\Documents\\GitHub\\Statistical-Analyze-Final\\Datasets\\y_corona.csv")
+write_xlsx(x_corona, "C:\\Users\\lenovo\\Documents\\GitHub\\R-Final\\Datasets\\y_corona.xlsx")
 
+library(readxl)
+y_corona <- read_excel("~/GitHub/R-Final/Datasets/y_corona.xlsx")
 
 #Gozlemi yapilacak veri Y_CORONA verisi
 
@@ -72,7 +73,6 @@ y_corona %>%
   theme_minimal() +
   theme(plot.title = element_text(face = "bold", hjust = 0.5))
 
-
 #Asianın Milyon kişi başına yeni ölüm sayısı YOĞUNLUK GRAFİĞİ
 library(dplyr)
 library(ggplot2)
@@ -87,7 +87,55 @@ y_corona %>%
  theme_minimal() +
  theme(plot.title = element_text(face = "bold", hjust = 0.5))
 
+#Asianın günlere göre yeni vaka sayısının BAR GRAFİĞİ:
+library(ggplot2)
+ggplot(Asia) +
+ aes(x = date, weight = new_cases) +
+ geom_bar(fill = "#B22222") +
+ labs(x = "Günler", y = "Vaka sayısı", 
+ title = "Haziran ayındaki yeni vaka sayıları", subtitle = "Asia") +
+ theme_minimal()
 
-#Asianın 
 
-esquisse::esquisser(data=y_corona)
+
+
+
+
+###Europe###
+#Europe'un toplam vaka / toplam ölüm ÇİZGİ GRAFİĞİ:
+library(dplyr)
+library(ggplot2)
+y_corona %>%
+ filter(continent %in% "Europe") %>%
+ ggplot() +
+ aes(x = total_cases, y = total_deaths) +
+ geom_line(size = 0.5, colour = "#B22222") +
+ labs(x = "Toplam vaka", y = "Toplam ölüm", title = "Toplam vaka / Toplam ölüm ") +
+ theme_minimal()
+
+#Europ'un Milyon kişi başına yeni ölüm sayısı YOĞUNLUK GRAFİĞİ
+library(dplyr)
+library(ggplot2)
+y_corona %>%
+ filter(continent %in% "Europe") %>%
+ ggplot() +
+ aes(x = new_deaths_per_million) +
+ geom_density(adjust = 1L, fill = "#FF8C00") +
+ scale_x_continuous(trans = "log") +
+ labs(x = "Milyon kişi ölüm oranı", y = "Yoğunluk", title = "Milyon kişi başına düşen yeni ölüm sayısı") +
+ theme_minimal()
+
+#Europe'un günlere göre yeni vaka sayısının BAR GRAFİĞİ:
+library(ggplot2)
+ggplot(Europe) +
+ aes(x = date, weight = new_cases) +
+ geom_bar(fill = "#228B22") +
+ theme_minimal()
+
+
+
+
+
+
+
+###North America###
